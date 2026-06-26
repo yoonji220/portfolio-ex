07b2e633c02812a6010c732e531c692c5d90e639
choiyui <choiyj220220@gmail.com>
2026-06-26 09:46:13 +0900

    feat: add aria-current attribute to Nav button for improved accessibility

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@ function Nav({ data, id, onChangeMode }) {
   const lists = data.map(work => (
     <li key={work.id}>
       <button
+        aria-current={work.id === id ? true : false}
         type="button"
         className={`btn ${
           work.id === id ? "btn-primary" : "btn-outline-primary"
